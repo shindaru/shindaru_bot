69d7e3943d6185d4cfe7496f7e25056453981d61
Radoslav Manolov <theroyalgamer15@gmail.com>
2022-06-13 09:49:51 +0300

    Initialize github repository

---
+++ main.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+require("dotenv").config();
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+});
+
+
+client.on("ready",() =>{
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+
+client.on("messageCreate",(message)=>{
+    if(message.content=="jsd"){
+        message.reply("Hello");
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.TOKEN);
