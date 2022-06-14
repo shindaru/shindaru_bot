558809daec3b22d6ac07d018fdf95d58f10b12e7
shindaru <theroyalgamer15@gmail.com>
2022-06-14 17:36:03 +0300

    Updates done by Radoslav

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const generateImage = require("./generateimage");
 require("dotenv").config();
-const Giphy = require("");
-
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
 });
@@ -24,23 +22,26 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on("messageCreate", (message) => {
-  let text = message.toString();
+client.on("message", async (msg) => {
+  let text = msg.toString();
   const tokens = msg.content.split(" ");
   if (text.includes(text.includes("rado!"))){
-    if (message.content == "no u") {
-      message.reply(`No,<@${message.member.id}> w :).`);
-    } else if(text.includes("rado!gif")){
-      const keywords = tokens.slice(1, tokens.length).join(" ")
+  if(tokens[0].toLowerCase()==="rado!gif"){
+    const keywords = tokens.slice(1,tokens.length).join(" ");
 
-      const url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}&limit10`;
-      const response = await fetch(url);
-      const result = await response.json();
-      const index = Math.floor(Math.random() * result.results.length);
-      message.reply(result.results[index].url);
-    }
-    else if (message.content == "no w") {
-      message.reply(`No,<@${message.member.id}> u :).`);
+    const url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}$limit=10`;
+    const response = await fetch(url);
+    const result = await response.json();
+    const index = Math.floor(Math.random()*result.results.length);
+
+    msg.reply(result.results[index.url])
+  }
+  if (msg.content == "no u") {
+      msg.reply(`No,<@${message.member.id}> w :).`);
+    } 
+
+    else if (msg.content == "no w") {
+      msg.reply(`No,<@${msg.member.id}> u :).`);
     }
      else {
       let izrecheniq = [
@@ -53,13 +54,8 @@ client.on("messageCreate", (message) => {
         "A da se shibash?",
       ];
       let repeat = izrecheniq[Math.floor(Math.random() * izrecheniq.length)];
-      message.reply(`<@${message.member.id}> ` + repeat);
+      msg.reply(`<@${msg.member.id}> ` + repeat);
     }
-  }
-});
-client.on("messageCreate", (message) => {
-  if (message == "gif") {
-    message.reply("I do not have that implemented yet.");
   }
 });
 
